7df1971dabf25b87ae35b43f5e59e7936ad66066
Hillel Arnold <hillel.arnold@gmail.com>
2023-06-30 13:57:33 -0400

    remove ecr login

--- containers/invalidate_cloudfront_cache.sh
+++ containers/invalidate_cloudfront_cache.sh
@@ -2,8 +2,6 @@
 
 # make sure correct region is set
 aws configure set default.region ${AWS_REGION}
-# Log in to AWS
-$(aws ecr get-login --no-include-email)
 
 aws cloudfront create-invalidation \
     --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} \
