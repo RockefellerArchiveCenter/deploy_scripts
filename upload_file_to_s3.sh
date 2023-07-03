e724d6bb005297e2b236539525d4a3d4fcfb675b
Hillel Arnold <hillel.arnold@gmail.com>
2023-07-03 13:18:44 -0400

    pass full filenames

--- upload_file_to_s3.sh
+++ upload_file_to_s3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-APP_NAME=$1
+FILE_NAME=$1
 BUCKET_NAME=$2
 
 # make sure correct region is set
 aws configure set default.region ${AWS_REGION}
 
-aws s3 mv ${APP_NAME}.template s3://${BUCKET_NAME}/${APP_NAME}.template
+aws s3 mv ${FILE_NAME} s3://${BUCKET_NAME}/${FILE_NAME}
 
