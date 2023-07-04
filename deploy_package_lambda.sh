081535cad844e2c0868b8d2500d8de400c0ac4e9
Hillel Arnold <hillel.arnold@gmail.com>
2023-07-04 11:42:42 -0400

    set region

--- deploy_package_lambda.sh
+++ deploy_package_lambda.sh
@@ -2,6 +2,9 @@
 
 LAMBDA_FUNCTION_NAME=$1
 
+# make sure correct region is set
+aws configure set default.region ${AWS_REGION}
+
 aws lambda update-function-code \
     --function-name  ${LAMBDA_FUNCTION_NAME} \
     --zip-file fileb://${LAMBDA_FUNCTION_NAME}.zip
