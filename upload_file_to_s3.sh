a1ed6b3e1b331aac9ac8ca7b101d4a7cf1621779
Hillel Arnold <hillel.arnold@gmail.com>
2023-07-04 11:31:10 -0400

    simplify

--- upload_file_to_s3.sh
+++ upload_file_to_s3.sh
@@ -3,9 +3,4 @@
 FILE_NAME=$1
 BUCKET_NAME=$2
 
-if [[ $FILE_NAME == *.zip ]]
-then
-     aws s3 mv --zip-file fileb://${FILE_NAME} s3://${BUCKET_NAME}/${FILE_NAME}
-else
-    aws s3 mv ${FILE_NAME} s3://${BUCKET_NAME}/${FILE_NAME}
-fi
+aws s3 cp ${FILE_NAME} s3://${BUCKET_NAME}/${FILE_NAME}
