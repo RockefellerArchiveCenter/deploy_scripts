adc6f113e3eb1050c9cb115d6b320cf0d10f7efe
Patrick <pjg250@gmail.com>
2024-10-07 11:08:56 -0400

    fix variable name
    Updates make_zip_lambda to remove `paths` from lambda handler code variable name

--- make_zip_lambda.sh
+++ make_zip_lambda.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
 LAMBDA_FUNCTION_NAME=$1
-LAMBDA_HANDLER_CODE_PATHS=$2
+LAMBDA_HANDLER_CODE=$2
 
 # install requirements if necessary
-if [ -f requirements.txt ]; 
+if [ -f requirements.txt ];
 then
   mkdir package
   source ~/virtualenv/python${TRAVIS_PYTHON_VERSION}/bin/activate
-  pip install -r requirements.txt --target ./package 
+  pip install -r requirements.txt --target ./package
   cd package
   zip -r ../${LAMBDA_FUNCTION_NAME}.zip .
   cd ..
